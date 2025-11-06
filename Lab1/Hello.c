1f7c873f62b41c5c736a2b757a3db6ac55af82b3
TuanLe-PS49449 <anhtuanlenguyen51@gmail.com>
2025-11-06 23:38:12 +0700

    init

---
+++ Lab1/Hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+    int main()
+{
+  int x; // Le Nguyen Anh Tuan - PS49449//
+  int y;
+
+  printf("Nhap vao hai so nguyen: ");
+  scanf("%d %d", &x, &y);
+
+  char name[30];
+  printf("nhap ten cua ban: ");
+  scanf("%s", &name);
+  return 0;
+}
