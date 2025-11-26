309add9454b551ce20ee4a8797d923387fcc4493
TuanLe-PS49449 <anhtuanlenguyen51@gmail.com>
2025-11-26 18:35:41 +0700

    Nộp bài tập Lab 5

--- Lab5/Bai_tap_lap5.c
+++ Lab5/Bai_tap_lap5.c
@@ -1,39 +1,58 @@
-//Bai 1: Tìm số lớn nhất trong 3 số đã cho 
+//Bai 1: Tìm số lớn nhất trong 3 số đã cho
 #include <stdio.h>
-#include <math.h>
 
-int main(){
-    printf("Nhap vao a,b,c: ");
-    double a,b,c;
-    scanf("%lf %lf %lf", &a, &b, &c);
+int main()
+{
+    int a, b, c, max;
 
-    double x = sqrt(a);
-    printf("Can bac 2 cua %lf", x);
-    double max =a;
-    if(max > b)
-        max = a;
-    else if(max < c)
+    printf("Nhap vao 3 so: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    max = a; // giả sử a là lớn nhất
+
+    if (b > max)
+        max = b;
+    if (c > max)
         max = c;
-    printf("\nMax la %lf", max);
-    //printf("\nMin la %lf", fmin(fmin(a,b),c));
+
+    printf("So lon nhat la: %d\n", max);
+
     return 0;
 }
 
 //Bài 2: Tính năm nhuận
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int year;
 
-    printf("Nhap nam: ");
+    printf("Nhap vao nam: ");
     scanf("%d", &year);
 
-    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
-        printf("%d la nam nhuan\n", year);
-    } else {
-        printf("%d khong phai nam nhuan\n", year);
-    }
+    if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+        printf("%d la nam nhuan.\n", year);
+    else
+        printf("%d khong phai nam nhuan.\n", year);
 
     return 0;
 }
 
+// Bài 3: Hoán vị 2 số x và y
+#include <stdio.h>
+
+int main()
+{
+    int x, y, temp;
+
+    printf("Nhap x va y: ");
+    scanf("%d %d", &x, &y);
+
+    temp = x;
+    x = y;
+    y = temp;
+
+    printf("Sau khi hoan vi: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
